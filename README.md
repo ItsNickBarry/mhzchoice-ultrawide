8629c08f6f219c495382689ea307ff14469faa05
Nick Barry <itsnickbarry@protonmail.ch>
2018-06-26 20:37:42 -0400

    initialize repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# MHzChoice Ultrawide
+A Firefox WebExtension which enables the toggling of ultrawide-scale video on the MHzChoice streaming service.
---
+++ mhzchoice_ultrawide.js
@@ -0,0 +1,41 @@
+var video, controlBar, fullscreenButton, resolutionsButton;
+var ultrawideButton;
+
+var findElements = function () {
+  video = document.querySelector('#video_html5_api');
+  controlBar = document.querySelectorAll('.vjs-control-bar')[2];
+
+  if (controlBar) {
+    fullscreenButton = controlBar.querySelector('.vjs-fullscreen-control');
+    resolutionsButton = controlBar.querySelector('.vjs-resolutions-button');
+  }
+
+  return !!(video && controlBar && fullscreenButton && resolutionsButton);
+};
+
+var toggleUltrawide = function () {
+  video.classList.toggle('ultrawide');
+  ultrawideButton.classList.toggle('pushed');
+};
+
+var run = function () {
+  if (findElements()) {
+    ultrawideButton = document.createElement('div');
+
+    ultrawideButton.classList.add('vjs-control')
+    ultrawideButton.classList.add('vjs-button')
+    ultrawideButton.classList.add('ultrawide');
+
+    ultrawideButton.title = 'Ultrawide';
+
+    ultrawideButton.addEventListener('click', toggleUltrawide);
+
+    controlBar.appendChild(fullscreenButton);
+    controlBar.appendChild(ultrawideButton);
+    controlBar.appendChild(resolutionsButton);
+  } else {
+    setTimeout(run, 100);
+  }
+};
+
+run();
